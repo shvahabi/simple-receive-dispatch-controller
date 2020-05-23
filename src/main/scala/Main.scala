d9f0bcf08f7749745173394d5c1b22329d931978
shvahabi <shvahabi@gmail.com>
2020-05-23 17:39:39 +0430

    v0.1.0 - selecting argonaut - merging case classes within Main, to better play with - adding XML representation of Forms - adding lots of new case classes - creating constructor for case classes - creating Json Codecs

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,4 @@
 import argonaut.{DecodeResult, Json, Parse}
-//import entity.Item
-//import io.circe.Json
-//import io.circe.parser._
-
-//import net.liftweb.json._
 
 object Main extends App {
 val aJSONString: String =
@@ -58,23 +53,51 @@ val aJSONString: String =
  |}
     """.stripMargin
 
-  /*
-  val itemInString: String =
-    """
-      |{
-      |"Title": "شیر",
-      |"Quantity": 2,
-      |"UnitOfMeasurement": "باکس",
-      |"GrossWeight": 55,
-      |"PackageWeight": 2,
-      |"NetWeight": 53
-      |}
-    """.stripMargin
-   */
+  case class GoodsInForm(FormNo: Int, BillOfLadingNo: String, PartNo: Int, Origin: String)
 
-  case class Item(Title: String, Quantity: String, UnitOfMeasurement: String, GrossWeight: String, PackageWeight: String, NetWeight: String)
+  case class GoodsOwner(FirstName: String, SurName: String, NationalIDNo: String) {
+    def toSql: String = f"INSERT INTO GoodsOwner (FirstName, SurName, NationalIDNo) VALUES ('${FirstName}', '${SurName}', '${NationalIDNo}')"
+  }
+  case class ReceivedDate(Year:Int, Month: Int, Day: Int) {
+    def toSql: String = f"INSERT INTO ReceivedDate(Year, Month, Day) VALUES ('${Year}', '${Month}', '${Day}')"
+  }
+  case class TruckNo(Iran: Int, TwoDigit: Int, ThreeDigit: Int, Alphabet: String) {
+    def toSql: String = f"INSERT INTO TruckNo(Year, Month, Day) VALUES ('${Iran}', '${TwoDigit}', '${ThreeDigit}', '${Alphabet}')"
+  }
+  case class Item(Title: String, Quantity: String, UnitOfMeasurement: String, GrossWeight: String, PackageWeight: String, NetWeight: String) {
+    def toSql: String = f"INSERT INTO ItemsList (Title, Quantity, Unit, GrossWeight, PackageWeight, NetWeight) VALUES ('${Title}','${Quantity}', '${UnitOfMeasurement}', '${GrossWeight}', '${PackageWeight}', '${NetWeight}')"
+  }
 
-  //val itemInJson: Json = Parse.parseOption(itemInString).get
+
+  def decodeGoodsInFormFromJson(inJson: Json): DecodeResult[GoodsInForm] =
+    for {
+      formNo <- (inJson.hcursor --\ "FormNo").as[Int]
+      billOfLadingNo <- (inJson.hcursor --\ "BillOfLadingNo").as[String]
+      partNo <- (inJson.hcursor --\ "PartNo").as[Int]
+      origin <- (inJson.hcursor --\ "Origin").as[String]
+    } yield GoodsInForm(formNo, billOfLadingNo, partNo, origin)
+
+  def decodeGoodsOwnerFromJson(inJson: Json): DecodeResult[GoodsOwner] =
+    for {
+      firstName <- (inJson.hcursor --\ "FirstName").as[String]
+      surName <- (inJson.hcursor --\ "SurName").as[String]
+      nationalIDNo <- (inJson.hcursor --\ "NationalIDNo").as[String]
+    } yield GoodsOwner(firstName, surName, nationalIDNo)
+
+  def decodeReceivedDateFromJson(inJson: Json): DecodeResult[ReceivedDate] =
+    for {
+      year <- (inJson.hcursor --\ "Year").as[Int]
+      month <- (inJson.hcursor --\ "Month").as[Int]
+      day <- (inJson.hcursor --\ "Day").as[Int]
+    } yield ReceivedDate(year, month, day)
+
+  def decodeTruckNoFromJson(inJson: Json): DecodeResult[TruckNo] =
+    for {
+      iran <- (inJson.hcursor --\ "Iran").as[Int]
+      twoDigit <- (inJson.hcursor --\ "TwoDigit").as[Int]
+      threeDigit <- (inJson.hcursor --\ "ThreeDigit").as[Int]
+      alphabet <- (inJson.hcursor --\ "Alphabet").as[String]
+    } yield TruckNo(iran, twoDigit, threeDigit, alphabet)
 
   def decodeItemFromJson(inJson: Json): DecodeResult[Item] =
     for {
@@ -86,146 +109,36 @@ val aJSONString: String =
       netWeight <- (inJson.hcursor --\ "NetWeight").as[Int]
     } yield Item(title, quantity.toString, unit, grossWeight.toString, packageWeight.toString, netWeight.toString)
 
-  //val anItem: Item = decodeItemFromJson(itemInJson).value.get
 
-  /*
-  val goodsOwnerInString: String =
-    """
-      {
-      "FirstName": "شاهد",
-      "SurName": "وهابی",
-      "NationalIDNo": "0061234567"
-      }
-    """.stripMargin
-   */
-
-  case class GoodsOwner (FirstName: String, SurName: String, NationalIDNo: String)
-
-  //val goodsOwnerInJson: Json = Parse.parseOption(goodsOwnerInString).get
-
-  def decodeGoodsOwnerFromJson(inJson: Json): DecodeResult[GoodsOwner] =
-    for {
-      firstName <- (inJson.hcursor --\ "FirstName").as[String]
-      surName <- (inJson.hcursor --\ "SurName").as[String]
-      nationalIDNo <- (inJson.hcursor --\ "NationalIDNo").as[String]
-    } yield GoodsOwner (firstName, surName, nationalIDNo)
-
-  //val anOwner: GoodsOwner = decodeGoodsOwnerFromJson(goodsOwnerInJson).value.get
-
-  /*
-  val aDateInString: String =
-    """
-      {
-      "Year": 2020,
-      "Month": 2,
-      "Day": 31
-      }
-    """.stripMargin
-   */
-
-  case class ReceivedDate(Year:Int, Month: Int, Day: Int)
-
-  //val receivedDateInJson: Json = Parse.parseOption(aDateInString).get
-
-  def decodeReceivedDateFromJson(inJson: Json): DecodeResult[ReceivedDate] =
-    for {
-      year <- (inJson.hcursor --\ "Year").as[Int]
-      month <- (inJson.hcursor --\ "Month").as[Int]
-      day <- (inJson.hcursor --\ "Day").as[Int]
-    } yield ReceivedDate(year, month, day)
-  //val aDate: ReceivedDate = decodeReceivedDateFromJson(receivedDateInJson).value.get
-
-  //println(aDate.Month + "/" + aDate.Day + "/" + aDate.Year)
-
-  /*
-  val aTruckNoInString: String =
-    """
-      |{
-      |    "Iran": 10,
-      |    "TwoDigit": 22,
-      |    "ThreeDigit": 792,
-      |    "Alphabet": "ی"
-      |  }
-    """.stripMargin
-   */
-
-  case class TruckNo(Iran: Int, TwoDigit: Int, ThreeDigit: Int, Alphabet: String)
-
-  //val truckNoInJson: Json = Parse.parseOption(aTruckNoInString).get
-
-  def decodeTruckNoFromJson(inJson: Json): DecodeResult[TruckNo] = {
-    for {
-      iran <- (inJson.hcursor --\ "Iran").as[Int]
-      twoDigit <- (inJson.hcursor --\ "TwoDigit").as[Int]
-      threeDigit <- (inJson.hcursor --\ "ThreeDigit").as[Int]
-      alphabet <- (inJson.hcursor --\ "Alphabet").as[String]
-    } yield TruckNo(iran, twoDigit, threeDigit, alphabet)
-  }
-
-  //val aTruckNo: TruckNo = decodeTruckNoFromJson(truckNoInJson).value.get
-  /*print("Iran" + aTruckNo.Iran + " " + aTruckNo.TwoDigit)
-  print(" " + aTruckNo.Alphabet + " ")
-  print(aTruckNo.ThreeDigit)*/
-
+  case class GoodsInFormGoodsOwnerRelation(FormNo: Int, FormGoodsOwner: GoodsOwner)
+  case class GoodsInFormDateRelation(FormNo: Int, FormReceivedDate: ReceivedDate)
   case class GoodsInFormTruckRelation(FormNo: Int, FormTruckNo: TruckNo)
-
-  def decodeGoodsInFormTruckRelation(inJson: Json): DecodeResult[GoodsInFormTruckRelation] =
-    for {
-      formNo <- (inJson.hcursor --\ "FormNo").as[Int]
-      formTruckNo <- decodeTruckNoFromJson((inJson.hcursor --\ "TruckNo").as[Json].value.get)
-    } yield GoodsInFormTruckRelation(formNo, formTruckNo)
-
-  //*****val aRelation: GoodsInFormTruckRelation = decodeGoodsInFormTruckRelation(Parse.parseOption(aJSONString).get).value.get
-
-
   case class GoodsInFormItemRelation(FormNo: Int, FormItems: List[Item])
 
 
+  val aJsonObject = Parse.parseOption(aJSONString).get
 
+  val aGoodsInForm: GoodsInForm = decodeGoodsInFormFromJson(aJsonObject).value.get
+  val aGoodsOwner: GoodsOwner =  decodeGoodsOwnerFromJson((aJsonObject.hcursor --\ "GoodsOwner").as[Json].value.get).value.get
+  val aReceivedDate: ReceivedDate = decodeReceivedDateFromJson((aJsonObject.hcursor --\ "ReceivedDate").as[Json].value.get).value.get
+  val aTruckNo: TruckNo = decodeTruckNoFromJson((aJsonObject.hcursor --\ "TruckNo").as[Json].value.get).value.get
+  val itemsList: List[Item] = (aJsonObject.hcursor --\ "ItemsList").as[List[Json]].value.get.map(xxx => decodeItemFromJson(xxx).value.get)
 
-  def decodeGoodsInFormItemRelation(inJson: Json, formItems: List[Item]): DecodeResult[GoodsInFormItemRelation] = {
-    for {
-      formNo <- (inJson.hcursor --\ "FormNo").as[Int]
-    } yield GoodsInFormItemRelation(formNo, formItems)
-  }
+  val aGoodsInFormGoodsOwnerRelation: GoodsInFormGoodsOwnerRelation = GoodsInFormGoodsOwnerRelation(aGoodsInForm.FormNo, aGoodsOwner)
+  val aGoodsInFormDateRelation: GoodsInFormDateRelation = GoodsInFormDateRelation(aGoodsInForm.FormNo, aReceivedDate)
+  val aGoodsInFormTruckRelation: GoodsInFormTruckRelation = GoodsInFormTruckRelation(aGoodsInForm.FormNo, aTruckNo)
+  val aGoodsInFormItemRelation: GoodsInFormItemRelation = GoodsInFormItemRelation(aGoodsInForm.FormNo, itemsList)
 
-  val inJson = Parse.parseOption(aJSONString).get
-  val itemsList: List[Item] = (inJson.hcursor --\ "ItemsList").as[List[Json]].value.get.map(sd => decodeItemFromJson(sd).value.get)
+  println("Form: ", aJsonObject)
+  println("Owner: ", aGoodsOwner)
+  println("Date: ", aReceivedDate)
+  println("Truck: ", aTruckNo)
+  println("Items: ", itemsList)
 
+  println()
 
-  println(    ((inJson.hcursor --\ "ItemsList").as[List[Json]].value.get).map(sd => decodeItemFromJson(sd).value.get) )
-  println(    decodeItemFromJson((inJson.hcursor --\ "ItemsList").as[List[Json]].value.get.head).value.get)
-
-
-  case class GoodsInFormDateRelation(FormNo: Int, FormReceivedDate: ReceivedDate)
-
-  def decodeGoodsInFormDateRelation(inJson: Json): DecodeResult[GoodsInFormDateRelation] =
-    for {
-      formNo <- (inJson.hcursor --\ "FormNo").as[Int]
-      formReceivedDate <- decodeReceivedDateFromJson((inJson.hcursor --\ "ReceivedDate").as[Json].value.get)
-    } yield GoodsInFormDateRelation(formNo, formReceivedDate)
-
-
-  case class GoodsInFormGoodsOwnerRelation(FormNo: Int, FormGoodsOwner: GoodsOwner)
-
-  def decodeGoodsInFormGoodsOwnerRelation(inJson: Json): DecodeResult[GoodsInFormGoodsOwnerRelation] =
-    for {
-      formNo <- (inJson.hcursor --\ "FormNo").as[Int]
-      formGoodsOwner <- decodeGoodsOwnerFromJson((inJson.hcursor --\ "GoodsOwner").as[Json].value.get)
-    } yield GoodsInFormGoodsOwnerRelation(formNo, formGoodsOwner)
-
-  case class GoodsInForm(FormNo: Int, BillOfLadingNo: String, PartNo: Int, Origin: String)
-
-  def decodeGoodsInFormFromJson(inJson: Json): DecodeResult[GoodsInForm] = {
-    for {
-      formNo <- (inJson.hcursor --\ "FormNo").as[Int]
-      billOfLadingNo <- (inJson.hcursor --\ "BillOfLadingNo").as[String]
-      partNo <- (inJson.hcursor --\ "PartNo").as[Int]
-      origin <- (inJson.hcursor --\ "Origin").as[String]
-    } yield GoodsInForm(formNo, billOfLadingNo, partNo, origin)
-  }
-
-
-
-
+  println(aGoodsInFormGoodsOwnerRelation)
+  println(aGoodsInFormDateRelation)
+  println(aGoodsInFormTruckRelation)
+  println(aGoodsInFormItemRelation)
 }
--- src/main/scala/entity/ReceivedGoodsForm.scala
+++
@@ -1,5 +0,0 @@
-package entity
-
-case class ReceivedGoodsForm(goodsOwner: GoodsOwner, receivedGoods: ReceivedGoods, itemsList: List[Item]) {
-  //def toSql: List[String] = List(goodsOwner.toSql, receivedGoods.toSql) :: itemsList.map(x => x.toSql)
-}
